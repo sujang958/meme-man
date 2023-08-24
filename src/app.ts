57fcb2a33781fbe6c1f07abf3ea56f28070d6a2c
sujang.958 <enter7377@naver.com>
2023-08-24 19:40:20 +0900

    fix: fix weird actions

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,8 @@
-import { XMLParser } from "fast-xml-parser"
-import { logWithDate, sendMeme } from "./utils.js"
+import { fetchMemes, logWithDate, sendMeme } from "./utils.js"
 import { CronJob } from "cron"
 
-const parser = new XMLParser()
-
-const REDDIT_URL_PATTERN = /https:\/\/i\.redd\.it[^"]*/g
-
-const fetchMeme = await fetch("https://www.reddit.com/r/ProgrammerHumor/.rss")
-const XMLdata = await fetchMeme.text()
-
-const obj = parser.parse(XMLdata)
-const entries = obj.feed.entry as any[]
-
-const images = entries
-  .map((entry) => String(entry.content).match(REDDIT_URL_PATTERN))
-  .filter((images): images is RegExpMatchArray => (images ? true : false))
-  .flat(1)
-
-const job = new CronJob("* 1 * * *", () => {
+const job = new CronJob("* 1 * * *", async () => {
+  const images = await fetchMemes()
   images.forEach((url, i) => {
     setTimeout(sendMeme.bind(null, url), i * 1000)
   })
--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,5 @@
 import "dotenv/config"
+import { XMLParser } from "fast-xml-parser"
 
 if (!("DISCORD_WEBHOOK_URL" in process.env)) process.exit(1)
 
@@ -26,4 +27,23 @@ export const sendMeme = async (url: string) => {
 
   logWithDate("Sending a meme")
   logWithDate(res.statusText, await res.text())
+}
+
+const parser = new XMLParser()
+
+const REDDIT_URL_PATTERN = /https:\/\/i\.redd\.it[^"]*/g
+
+export const fetchMemes = async () => {
+  const fetchMeme = await fetch("https://www.reddit.com/r/ProgrammerHumor/.rss")
+  const XMLdata = await fetchMeme.text()
+
+  const obj = parser.parse(XMLdata)
+  const entries = obj.feed.entry as any[]
+
+  const images = entries
+    .map((entry) => String(entry.content).match(REDDIT_URL_PATTERN))
+    .filter((images): images is RegExpMatchArray => (images ? true : false))
+    .flat(1)
+
+  return images
 }
