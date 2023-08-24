70942cf2b20496fa3d2871a1594605d2bad0639f
sujang.958 <enter7377@naver.com>
2023-08-24 23:22:58 +0900

    fix: fix wrong cron notations'

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 import { fetchMemes, logWithDate, sendMeme } from "./utils.js"
 import { CronJob } from "cron"
 
-const job = new CronJob("* 1 * * *", async () => {
+const job = new CronJob("0 * * * *", async () => {
   const images = await fetchMemes()
+
   images.forEach((url, i) => {
     setTimeout(sendMeme.bind(null, url), i * 1000)
   })
